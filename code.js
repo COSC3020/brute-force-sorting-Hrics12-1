3dbbe5b05a87fcc4f791a1c5c2fc237ecff51ef5
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-11-19 15:54:37 -0700

    Update code.js

--- code.js
+++ code.js
@@ -22,7 +22,7 @@ function permute(a,l,r,count){
 
 
 function isSorted(a){
-  for (let i =0; i < a.length; i++){
+  for (let i =0; i < a.length-1; i++){
     if (a[i] > a[i + 1]){
       return false;
     }
